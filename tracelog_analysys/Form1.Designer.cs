19d47da12ad88d6cbd601f8af374421e721e3de6
Administrator <admin@example.com>
2017-09-15 13:17:39 +0900

    データ部検出、出力機能作成

--- tracelog_analysys/Form1.Designer.cs
+++ tracelog_analysys/Form1.Designer.cs
@@ -28,12 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.logWindowTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(3, 3);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(168, 23);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "読込";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(3, 27);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(502, 19);
+            this.fileNameTextBox.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルFToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(860, 26);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルFToolStripMenuItem
+            // 
+            this.ファイルFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開くToolStripMenuItem});
+            this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
+            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(85, 22);
+            this.ファイルFToolStripMenuItem.Text = "ファイル(F)";
+            // 
+            // 開くToolStripMenuItem
+            // 
+            this.開くToolStripMenuItem.Name = "開くToolStripMenuItem";
+            this.開くToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.開くToolStripMenuItem.Text = "開く";
+            this.開くToolStripMenuItem.Click += new System.EventHandler(this.開くToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "対象ファイル";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 541);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(860, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.Control;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 352F));
+            this.tableLayoutPanel1.Controls.Add(this.fileNameTextBox, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 26);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 17F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(860, 50);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.loadButton, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(160, 78);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(349, 59);
+            this.tableLayoutPanel2.TabIndex = 6;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(5, 78);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(149, 460);
+            this.treeView1.TabIndex = 7;
+            // 
+            // logWindowTextBox
+            // 
+            this.logWindowTextBox.Location = new System.Drawing.Point(160, 143);
+            this.logWindowTextBox.Multiline = true;
+            this.logWindowTextBox.Name = "logWindowTextBox";
+            this.logWindowTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.logWindowTextBox.Size = new System.Drawing.Size(346, 395);
+            this.logWindowTextBox.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(512, 143);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(92, 395);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(610, 143);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(238, 395);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(511, 27);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(346, 19);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(511, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "対象IP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(860, 563);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.logWindowTextBox);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "tracelog_analysys";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem 開くToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox logWindowTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- tracelog_analysys/Form1.cs
+++ tracelog_analysys/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,5 +16,149 @@ namespace tracelog_analysys
         {
             InitializeComponent();
         }
+
+        //開くメニュー実行時
+        private void 開くToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //はじめのファイル名を指定
+            //ofd.FileName = "";
+            //はじめに表示されるフォルダを指定する
+            //ofd.InitialDirectory = @"C:\";
+            ofd.InitialDirectory = System.Environment.CurrentDirectory;
+            //[ファイルの種類]に表示される選択肢を指定する
+            ofd.Filter = "すべてのファイル(*.*)|*.*";
+            //[ファイルの種類]ではじめに選択されるものを指定する
+            ofd.FilterIndex = 1;
+            //タイトルを設定する
+            ofd.Title = "開くファイルを選択してください";
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき、選択されたファイル名を表示する
+                fileNameTextBox.Text = ofd.FileName;
+            }
+        }
+
+        //読込ボタン
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(
+                fileNameTextBox.Text,Encoding.GetEncoding("utf-8"));
+
+            int currentIndex = 0;
+            int num_i = 0;
+            int num_j = 0;
+            int num_k = 0;
+            int startIndex = 0;
+            int endIndex = 0;
+            int reverseIndex = 0;
+            int dataFlag = 0;
+            int tmpSize = 100;
+            string[] stArray = new string[tmpSize];
+            string text;
+
+            //while(!sr.EndOfStream)
+            for (num_i = 0; num_i < 60000; num_i++)
+            {
+                text = sr.ReadLine() + "\n";
+                
+                //検索し、該当したら行とデータ部を抜き出す
+                if (0 <= text.IndexOf("10.22.227.7"))
+                {
+                    //データ部はバッファ配列を遡って確認する
+                    for (num_j = 0; num_j < tmpSize; num_j++)
+                    {
+                        reverseIndex = currentIndex - num_j;
+                        if(reverseIndex < 0){
+                            reverseIndex += tmpSize;
+                        }
+
+                        //データ部は改行で区切られているものとする
+                        if (dataFlag == 0)
+                        {
+                            //一つ目の改行　データ部開始フェイズ
+                            if (stArray[reverseIndex] == "\n")
+                            {
+                                dataFlag = 1;
+                                startIndex = reverseIndex;
+                            }
+                        }
+                        else if (dataFlag == 1)
+                        {
+                            //二つ目の改行　データ部終了フェイズ
+                            if (stArray[reverseIndex] == "\n")
+                            {
+                                dataFlag = 2;
+                                endIndex = reverseIndex;
+                            }
+                        }
+                        else if (dataFlag == 2)
+                        {
+                            //書き込みフェイズ
+                            //バッファを遡る
+                            if(endIndex < startIndex){
+                                for (num_k = endIndex; num_k <= startIndex; num_k++)
+                                {
+                                    textBox2.AppendText(stArray[num_k]);
+                                }
+                            }else{
+                                //バッファが一周している時は2段階で遡る
+                                for (num_k = endIndex; num_k <= tmpSize-1; num_k++)
+                                {
+                                    textBox2.AppendText(stArray[num_k]);
+                                }
+
+                                for (num_k = 0; num_k <= startIndex; num_k++)
+                                {
+                                    textBox2.AppendText(stArray[num_k]);
+                                }
+                            }
+
+                            dataFlag = 0;
+                            break;
+                        }
+
+                    }
+                    reverseIndex = 0;
+                    textBox2.AppendText(text + "\n");
+                }
+
+                //バッファ配列に格納
+                stArray[currentIndex] = text;
+
+                //確認用
+                //logWindowTextBox.AppendText(text);
+                //textBox1.AppendText(currentIndex.ToString() + "\n");
+
+                //ループ処理
+                if (currentIndex < tmpSize-1)
+                {
+                    currentIndex++;
+                }
+                else
+                {
+                    currentIndex = 0;
+                }
+                
+            }
+            
+            sr.Close();
+        }
     }
+    
+
+
+
+    /// <summary>
+    /// ファイル関連のクラス
+    /// </summary>
+
+    //public class FileManage
+    //{
+
+    //}
 }
